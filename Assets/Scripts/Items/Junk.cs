51cde6935abc2f5767e542b73b4c50e6b4e4cdf6
bumblebeangames <bumblebeangames@gmail.com>
2024-05-10 17:05:20 -0500

    Finished region 2 levels

--- Assets/Scripts/Items/Junk.cs
+++ Assets/Scripts/Items/Junk.cs
@@ -4,15 +4,55 @@ using UnityEngine;
 
 public class Junk : MonoBehaviour
 {
-
     public ItemTypes itemType;
     public ColorTypes colorType;
 
-    private void OnCollisionEnter2D(Collision2D collision)
+    Rigidbody2D _rigidBody;
+
+    bool _isOffScreen;
+    float _offscreenTimer;
+    float _timeUntilDestroy = 5f;
+
+    float _rotationSpeed;
+    float _minRotationSpeed = -80.0f;
+    float _maxRotationSpeed = 80.0f;
+
+
+
+    private void Awake()
     {
-        if (collision.gameObject.layer == Tags.Ground)
+        _rigidBody = GetComponent<Rigidbody2D>();
+        _rotationSpeed = Random.Range(_minRotationSpeed, _maxRotationSpeed);
+        float _randomRotation = Random.Range(0, 360f);
+        transform.rotation = Quaternion.Euler(0f, 0f, _randomRotation);
+        _rigidBody.angularVelocity = _rotationSpeed;
+
+    }
+
+    private void Update()
+    {
+        if(_isOffScreen)
+        {
+            _offscreenTimer += Time.deltaTime;
+        }
+
+        if(_offscreenTimer > _timeUntilDestroy)
         {
             Destroy(gameObject);
         }
+
+
+        //transform.Rotate(new Vector3(0, 0, Time.deltaTime * _rotationSpeed));
+    }
+
+    private void OnBecameInvisible()
+    {
+        _isOffScreen = true;
+    }
+
+    private void OnBecameVisible()
+    {
+        _isOffScreen = false;
+        _offscreenTimer = 0;
     }
 }
